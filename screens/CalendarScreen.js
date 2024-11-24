e4d4cc8a1fcf353da06f846429da7f2ccfc8b2e2
Dallybearr <Dallasmaxim15@icloud.com>
2024-11-24 12:04:08 -0700

    added calendar page

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import Calendar from 'react-calendar';
+import '../screens/styles/Calendar.css'; // Ensure the correct path to your CSS file
 
 const CalendarScreen = () => {
+  const [value, onChange] = React.useState(new Date());
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Calendar</Text>
+    <View style={{ flex: 1 }}>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      
+        <View style={styles.container}>
+          <Calendar
+            onChange={onChange}
+            value={value}
+            className="custom-calendar"
+          />
+        </View>
+      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
+    justifyContent: 'flex-start', 
+    alignItems: 'center', 
+    width: '100%',
+    height: '100%',
     backgroundColor: '#fff',
   },
-  title: {
-    fontSize: 24,
-    textAlign: 'center',
-  },
+  
+  
+  
 });
 
 export default CalendarScreen;
