e0665d36c5745b903ae79e28cf759cf4c97906c5
Dally-Bear <152315550+Dally-Bear@users.noreply.github.com>
2024-11-24 13:57:31 -0700

    Update CalendarScreen.js

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Calendar from 'react-calendar';
-import '../screens/styles/Calendar.css'; // Ensure the correct path to your CSS file
+import '../screens/styles/Calendar.css'; 
 
 const CalendarScreen = ({ navigation }) => {
   const [selectedDate, setSelectedDate] = React.useState(null);
