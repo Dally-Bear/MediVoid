75cde5d91a28294c68fadfbbeec2e03761165460
Abduallah-Shaklaoon <abduallah.shaklaoon@edu.sait.ca>
2024-12-02 02:36:45 -0700

    added graphics, and tidied up ultrasound page

--- screens/UltrasoundScreen.js
+++ screens/UltrasoundScreen.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button } from 'react-native-elements';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { Button } from "react-native-elements";
 
 const UltrasoundScreen = () => {
   const [randomNumber, setRandomNumber] = useState(0);
+  const [ultraImage, setUltraImage] = useState(require('../assets/scannerposition.png'));
+  const [detailText, setDetailText] = useState('Position the scanner relative to the diagram depicted above, when ready press the button to start the scan.');
 
   const randNumberGenerator = () => {
     let ultraNum = Math.floor(Math.random() * 350 + 1);
     if (ultraNum > 100) {
       ultraNum = Math.floor(Math.random() * 350 + 1);
+      if (ultraNum > 100) {
+        ultraNum = Math.floor(Math.random() * 350 + 1);
+      }
     }
     return ultraNum;
   };
@@ -16,12 +21,29 @@ const UltrasoundScreen = () => {
   const handleClick = () => {
     const newNumber = randNumberGenerator();
     setRandomNumber(newNumber);
+    if (newNumber < 50) {
+      setUltraImage(require('../assets/void.png'));
+      setDetailText('The scan is complete, results show bladder is mostly voided.');
+    } else if (newNumber < 150) {
+      setUltraImage(require('../assets/partial.png'));
+      setDetailText('The scan is complete, results show bladder is partially voided.');
+    } else {
+      setUltraImage(require('../assets/full.png'));
+      setDetailText('The scan is complete, results show bladder bladder is not being voided enough, if the issue persists please consult a doctor');
+    }
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Random Number: {randomNumber}</Text>
-      <Button title="Generate Number" onPress={handleClick} />
+      <Text style={styles.title}>Ultrasound Screen</Text>
+      <Image
+        source={ultraImage}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.directions}>{detailText}</Text>
+      <Text style={styles.title}>{randomNumber}</Text>
+      <Button style ={styles.button} title="Start Scan" onPress={handleClick} />
     </View>
   );
 };
@@ -29,15 +51,36 @@ const UltrasoundScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
-    backgroundColor: '#fff',
+    paddingVertical: 90,
+    backgroundColor: "#fff",
+  },
+  directions: {
+    fontSize: 16,
+    textAlign: "center",
+    padding: 20,
+    marginBottom: 20,
+    backgroundColor: "#ebf5ff",
+    borderRadius: 5,
   },
   title: {
     fontSize: 24,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 20,
+  },
+  logo: {
+    width: "250px",
+    height: "250px",
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007bff', 
+    padding: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 16,
   },
 });
 
